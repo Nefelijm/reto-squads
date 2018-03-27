a46239c0cbba54b17f249976bd4c5f5cc7e0ce54
carlacentenor <carlacentenor@gmail.com>
2018-03-27 16:38:41 -0500

    insertando tabla

--- public/js/app.js
+++ public/js/app.js
@@ -255,7 +255,15 @@ function organizar() {
       $(pintarSquad(newSq6, '#sq6'));
       $(pintarSquad(newSq7, '#sq7'));
       $(pintarSquad(newSq8, '#sq8'));
-      // $(pintarSquad(newSq9, '#sq9'));
+      
+      localStorage.setItem('squad1', JSON.stringify(newSq1));
+      localStorage.setItem('squad2', JSON.stringify(newSq2));
+      localStorage.setItem('squad3', JSON.stringify(newSq3));
+      localStorage.setItem('squad4', JSON.stringify(newSq4));
+      localStorage.setItem('squad5', JSON.stringify(newSq5));
+      localStorage.setItem('squad6', JSON.stringify(newSq6));
+      localStorage.setItem('squad7', JSON.stringify(newSq7));
+      localStorage.setItem('squad8', JSON.stringify(newSq8));
     });
   });
 }
@@ -286,6 +294,7 @@ function bubble(arr) {
   }
   return arr;
 }
+
 
 // /******DRAG AND DROP*************/
 
@@ -549,4 +558,69 @@ function Warming() {
   if (!validate1) {
     box.innerHTML += message1;
   }
+}
+
+
+/** ****Table */
+
+$('#btnExport').click(function(event) {
+  window.open('data:application/vnd.ms-excel,' + $('#dvData').html());
+  event.preventDefault();
+});
+
+// obteniendo squads:
+
+var squad1t = JSON.parse(localStorage.squad1);
+var squad2t = JSON.parse(localStorage.squad2);
+var squad3t = JSON.parse(localStorage.squad3);
+var squad4t = JSON.parse(localStorage.squad4);
+var squad5t = JSON.parse(localStorage.squad5);
+var squad6t = JSON.parse(localStorage.squad6);
+var squad7t = JSON.parse(localStorage.squad7);
+var squad8t = JSON.parse(localStorage.squad8);
+
+var array1 = []; 
+var array2 = [];
+var array3 = []; 
+var array4 = [];
+var array5 = [];
+var array6 = [];
+var array7 = [];
+var array8 = [];      
+dbRefObject.on('value', function(snap) {
+  var nameStudents = snap.val();
+
+  var colum1 = [squad1t[0], squad2t[0], squad3t[0], squad4t[0], squad5t[0], squad6t[0], squad7t[0], squad8t[0]];
+  var colum2 = [squad1t[1], squad2t[1], squad3t[1], squad4t[1], squad5t[1], squad6t[1], squad7t[1], squad8t[1]];
+  var colum3 = [squad1t[2], squad2t[2], squad3t[2], squad4t[2], squad5t[2], squad6t[2], squad7t[2], squad8t[2]];
+  var colum4 = [squad1t[3], squad2t[3], squad3t[3], squad4t[3], squad5t[3], squad6t[3], squad7t[3], squad8t[3]];
+  var colum5 = [squad1t[4], squad2t[4], squad3t[4], squad4t[4], squad5t[4], squad6t[4], squad7t[4], squad8t[4]];
+  var colum6 = [squad1t[5], squad2t[5], squad3t[5], squad4t[5], squad5t[5], squad6t[5], squad7t[5], squad8t[5]];
+  var colum7 = [squad1t[6], squad2t[6], squad3t[6], squad4t[6], squad5t[6], squad6t[6], squad7t[6], squad8t[6]];
+  var colum8 = [squad1t[7], squad2t[7], squad3t[7], squad4t[7], squad5t[7], squad6t[7], squad7t[7], squad8t[7]];
+
+  memberSquad(colum1, array1, nameStudents, '#squad1t') ;
+
+  memberSquad(colum2, array2, nameStudents, '#squad2t') ;
+  memberSquad(colum3, array3, nameStudents, '#squad3t') ;
+  memberSquad(colum4, array4, nameStudents, '#squad4t') ;
+  memberSquad(colum5, array5, nameStudents, '#squad5t') ;
+  memberSquad(colum6, array6, nameStudents, '#squad6t') ;
+  memberSquad(colum7, array7, nameStudents, '#squad7t') ;
+  memberSquad(colum8, array8, nameStudents, '#squad8t') ;
+});
+
+
+function memberSquad(squad, array, arrayTotal, dom) {
+  for (p = 0; p < squad.length;p++) {
+    for (q = 0; q < arrayTotal.length ; q++) {
+      if (squad[p] === arrayTotal[q].index) {
+        array.push(arrayTotal[q].name);
+      }
+    }
+  }
+  array.map(element => {
+    var template = `<td>${element.first} ${element.last}</td>`;
+    $(dom).append(template);
+  });
 }
