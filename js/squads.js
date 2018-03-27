e2717ac771ba7a4e23146ea2e8524d16831d7e9a
darcycervh <darcycervera@gmail.com>
2018-03-27 02:08:31 -0500

    añadiendo detalles

--- js/squads.js
+++ js/squads.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-  $(document).ready(function(){
-    $('.modal').modal();
-  });
-})
-=======
 $(document).ready(function() {
   $(document).ready(function() {
     $('.modal').modal();
   });
 });
->>>>>>> 9d376558ea7e60224cfe637e3943cf93e5132166
