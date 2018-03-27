536981f2e76fc4fc5242bf66d6e0410a9007737f
Nefelijm <sistemasnik20@gmail.com>
2018-03-27 09:41:13 -0500

    Instalando server para las gmail

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.listen(5000, () => {
+app.listen(4000, () => {
   console.log('servidor encendido');
 });
 app.use(express.static('public'));
